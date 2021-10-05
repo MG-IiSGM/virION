a58462308ecb082a6f5bc1e0ac1931260e1eaa78
laura <sergio.buenestado@gmail.com>
2021-10-05 13:16:40 +0200

    Ommiting guppy

--- guppy_covidion.py
+++ guppy_covidion.py
@@ -49,34 +49,42 @@ DIM = '\033[2m'
 
 def get_arguments():
 
+    parser = argparse.ArgumentParser(
+        prog='guppy_minion.py', description='Pipeline to basecalling and barcoding fast5 files from minION sequencing')
 
-    parser = argparse.ArgumentParser(prog = 'guppy_minion.py', description = 'Pipeline to basecalling and barcoding fast5 files from minION sequencing')
+    parser.add_argument('-i', '--input', dest='input_dir', metavar='input_directory',
+                        type=str, required=True, help='REQUIRED. Input directory containing all fast5 files')
 
-    parser.add_argument('-i', '--input', dest = 'input_dir', metavar = 'input_directory', type = str, required = True, help = 'REQUIRED. Input directory containing all fast5 files')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='REQUIRED. Output directory to extract all results')
 
-    parser.add_argument('-o', '--output', type = str, required = True, help = 'REQUIRED. Output directory to extract all results')
+    parser.add_argument('-s', '--samples', metavar='Samples', type=str, required=False,
+                        help='Sample list for conversion from barcode to samples ID')
 
-    parser.add_argument('-s', '--samples', metavar = 'Samples', type = str, required = False, help = 'Sample list for conversion from barcode to samples ID')
+    parser.add_argument('-c', '--config', type=str, default='dna_r9.4.1_450bps_fast.cfg', required=False,
+                        help='REQUIRED. Config parameter for guppy_basecalling. High-accuracy mode basecalling by default')
 
-    parser.add_argument('-c', '--config', type = str, default = 'dna_r9.4.1_450bps_fast.cfg', required = False, help = 'REQUIRED. Config parameter for guppy_basecalling. High-accuracy mode basecalling by default')
+    parser.add_argument('-b', '--require_barcodes_both_ends', required=False, action='store_true',
+                        help='Require barcodes at both ends. By default it only requires the barcode at one end for the sequences identification')
 
-    parser.add_argument('-b', '--require_barcodes_both_ends', required = False, action = 'store_true', help = 'Require barcodes at both ends. By default it only requires the barcode at one end for the sequences identification')
+    parser.add_argument('--barcode_kit', type=str, required=False,
+                        default='EXP-NBD196', help='Kit of barcodes used')
 
-    parser.add_argument('--barcode_kit', type = str, required = False, default = 'EXP-NBD196', help = 'Kit of barcodes used')
+    parser.add_argument('-t', '--threads', type=int, dest='threads', required=False,
+                        default=30, help='Threads to use (30 threads by default)')
 
-    parser.add_argument('-t', '--threads', type = int, dest = 'threads', required = False, default = 30, help = 'Threads to use (30 threads by default)')
+    parser.add_argument('--num_callers', type=int, dest='num_callers',
+                        required=False, default=10, help='Number of parallel basecallers')
 
-    parser.add_argument('--num_callers', type = int, dest = 'num_callers', required = False, default = 10, help = 'Number of parallel basecallers')
-
-    parser.add_argument('--records_per_fastq', type = int, dest = 'records_per_fastq', required = False, default = 0, help = 'Maximum number of records per fastq')
-
+    parser.add_argument('--records_per_fastq', type=int, dest='records_per_fastq',
+                        required=False, default=0, help='Maximum number of records per fastq')
 
     arguments = parser.parse_args()
 
     return arguments
 
 
-def basecalling_ion(input_dir, out_basecalling_dir, config = 'dna_r9.4.1_450bps_fast.cfg', callers = 3, chunks = 2048, threads = 10, records = 0):
+def basecalling_ion(input_dir, out_basecalling_dir, config='dna_r9.4.1_450bps_fast.cfg', callers=3, chunks=2048, threads=10, records=0):
 
     # -i: Path to input fast5 files
     # -s: Path to save fastq files
@@ -85,14 +93,16 @@ def basecalling_ion(input_dir, out_basecalling_dir, config = 'dna_r9.4.1_450bps_
     # --cpu_threads_per_caller: Number of CPU worker threads per basecaller
     # --chunks_per_runner: Maximum chunks per runner
     # --compress_fastq: Compress fastq output files with gzip
+    # --records_per_fastq: Maximum number of records per fastq file, 0 means use a single file (per worker, per run id)
 
-    cmd = ['guppy_basecaller', '-i', input_dir, '-s', out_basecalling_dir, '-c', config, '--num_callers', str(callers), '--chunks_per_runner', str(chunks), '--cpu_threads_per_caller', str(threads), '--records_per_fastq', str(records), '--compress_fastq']
+    cmd = ['guppy_basecaller', '-i', input_dir, '-s', out_basecalling_dir, '-c', config, '--num_callers',
+           str(callers), '--chunks_per_runner', str(chunks), '--cpu_threads_per_caller', str(threads), '--records_per_fastq', str(records), '--compress_fastq']
 
     print(cmd)
-    execute_subprocess(cmd, isShell = False)
+    execute_subprocess(cmd, isShell=False)
 
 
-def barcoding_ion(out_basecalling_dir, out_barcoding_dir, require_barcodes_both_ends = False, barcode_kit = 'EXP-NBD196', threads = 30):
+def barcoding_ion(out_basecalling_dir, out_barcoding_dir, require_barcodes_both_ends=False, barcode_kit='EXP-NBD196', threads=30):
 
     # -i: Path to input files
     # -r: Search for input file recursively
@@ -102,23 +112,24 @@ def barcoding_ion(out_basecalling_dir, out_barcoding_dir, require_barcodes_both_
     # --compress_fastq: Compress fastq output files with gzip
     # --barcode_kits: Space separated list of barcoding kit(s) or expansion kit(s) to detect against. Must be in double quotes
     # --require_barcodes_both_ends: Reads will only be classified if there is a barcode above the min_score at both ends of the read
-    # --records_per_fastq: Maximum number of records per fastq file, 0 means use a single file (per worker, per run id)
 
     if require_barcodes_both_ends:
         logger.info(GREEN + BOLD + 'Barcodes are being used at both ends')
         require_barcodes_both_ends = "--require_barcodes_both_ends"
     else:
-        logger.info(YELLOW + DIM + BOLD + 'Barcodes are being used on at least 1 of the ends')
+        logger.info(YELLOW + DIM + BOLD +
+                    'Barcodes are being used on at least 1 of the ends')
         require_barcodes_both_ends = ""
 
-    cmd = ['guppy_barcoder', '-i', out_basecalling_dir, '-s', out_barcoding_dir, '-r', require_barcodes_both_ends, '--barcode_kit', barcode_kit, '-t', str(threads), '--fastq_out', '--compress_fastq']
+    cmd = ['guppy_barcoder', '-i', out_basecalling_dir, '-s', out_barcoding_dir, '-r', require_barcodes_both_ends,
+           '--barcode_kit', barcode_kit, '-t', str(threads), '--fastq_out', '--compress_fastq']
 
     print(cmd)
-    execute_subprocess(cmd, isShell = False)
+    execute_subprocess(cmd, isShell=False)
 
 
-def read_filtering(out_barcoding_dir, out_samples_dir, summary = False, min_length = 270, max_length = 550):
-    
+def read_filtering(out_barcoding_dir, out_samples_dir, summary=False, min_length=270, max_length=550):
+
     # --directory:
     # --prefix:
     # --min-length:
@@ -127,20 +138,21 @@ def read_filtering(out_barcoding_dir, out_samples_dir, summary = False, min_leng
 
     with open(summary, 'r') as f:
         for line in f:
-            barcode = os.path.join(out_barcoding_dir, line.split('\t')[0].strip())
+            barcode = os.path.join(
+                out_barcoding_dir, line.split('\t')[0].strip())
             sample = line.split('\t')[1].strip()
             output_samples = os.path.join(out_samples_dir, sample + '.fastq')
             # print(output_samples)
 
-            cmd = ['artic', 'guppyplex', '--directory', barcode, '--prefix', sample, '--min-length', str(min_length), '--max-length', str(max_length), '--output', output_samples]
+            cmd = ['artic', 'guppyplex', '--directory', barcode, '--prefix', sample, '--min-length',
+                   str(min_length), '--max-length', str(max_length), '--output', output_samples]
 
             # print(cmd)
-            execute_subprocess(cmd, isShell = False, isInfo = True)
+            execute_subprocess(cmd, isShell=False, isInfo=True)
 
             # cmd_compress = ['bgzip', output_samples, '--threads', str(args.threads)]
             # print(cmd_compress)
             # execute_subprocess(cmd_compress, isShell = False)
-
 
 
 if __name__ == '__main__':
@@ -178,14 +190,13 @@ if __name__ == '__main__':
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
 
-    logger.info("\n" + BLUE + '############### START PROCESSING FAST5 FILES ###############' + END_FORMATTING + "\n")
+    logger.info(
+        "\n" + BLUE + '############### START PROCESSING FAST5 FILES ###############' + END_FORMATTING + "\n")
     logger.info(args)
-
 
     # Obtain all fast5 files from folder
 
     fast5 = extract_read_list(args.input_dir)
-
 
     # Check how many files will be analysed
 
@@ -195,43 +206,53 @@ if __name__ == '__main__':
         sample = extract_sample_list(sample)
         sample_list.append(sample)
 
-    logger.info("\n" + CYAN + "{} Samples will be analysed: {}".format(len(sample_list), ",".join(sample_list)) + END_FORMATTING)
-
+    logger.info("\n" + CYAN + "{} Samples will be analysed: {}".format(
+        len(sample_list), ",".join(sample_list)) + END_FORMATTING)
 
     # Declare folders created in pipeline and key files
 
     out_basecalling_dir = os.path.join(output_dir, 'Basecalling')
     check_create_dir(out_basecalling_dir)
+    basecalling_summary = os.path.join(
+        out_basecalling_dir, 'sequencing_summary.txt')
     out_barcoding_dir = os.path.join(output_dir, 'Barcoding')
     check_create_dir(out_barcoding_dir)
+    barcoding_summary = os.path.join(
+        out_barcoding_dir, 'barcoding_summary.txt')
     out_samples_dir = os.path.join(output_dir, 'Samples_Fastq')
     check_create_dir(out_samples_dir)
     out_fastqc_dir = os.path.join(output_dir, 'Quality')
     check_create_dir(out_fastqc_dir)
 
-
     ############### Start pipeline ###############
 
     # Basecalling
 
-    logger.info("\n" + GREEN + "STARTING BASECALLING" + END_FORMATTING + "\n")
-
-    basecalling_ion(input_dir, out_basecalling_dir, config = args.config, callers = args.num_callers, chunks = 2048, threads = args.threads, records = args.records_per_fastq)
-
+    if os.path.isfile(basecalling_summary):
+        logger.info("\n" + YELLOW + "Ommiting BASECALLING" +
+                    END_FORMATTING + "\n")
+    else:
+        logger.info("\n" + GREEN + "STARTING BASECALLING" +
+                    END_FORMATTING + "\n")
+        basecalling_ion(input_dir, out_basecalling_dir, config=args.config, callers=args.num_callers,
+                        chunks=2048, threads=args.threads, records=args.records_per_fastq)
 
     # Barcoding
 
-    logger.info("\n" + GREEN + "STARTING BARCODING/DEMULTIPLEX" + END_FORMATTING + "\n")
-
-    barcoding_ion(out_basecalling_dir, out_barcoding_dir, barcode_kit = args.barcode_kit, threads = args.threads, require_barcodes_both_ends = args.require_barcodes_both_ends)
-
+    if os.path.isfile(barcoding_summary):
+        logger.info("\n" + YELLOW + "Ommiting BARCODING/DEMULTIPLEX" +
+                    END_FORMATTING + "\n")
+    else:
+        logger.info("\n" + GREEN + "STARTING BARCODING/DEMULTIPLEX" +
+                    END_FORMATTING + "\n")
+        barcoding_ion(out_basecalling_dir, out_barcoding_dir, barcode_kit=args.barcode_kit,
+                      threads=args.threads, require_barcodes_both_ends=args.require_barcodes_both_ends)
 
     # Read Filtering
 
     logger.info("\n" + GREEN + "SAMPLE FILTERING" + END_FORMATTING + "\n")
 
-    read_filtering(out_barcoding_dir, out_samples_dir, summary = args.samples)
-
+    read_filtering(out_barcoding_dir, out_samples_dir, summary=args.samples)
 
     # Quality Check
 
@@ -245,15 +266,17 @@ if __name__ == '__main__':
         sample_base = os.path.basename(sample)
         sample_fastq.append(os.path.basename(sample))
 
-        out_qc_raw_fastq = ('.').join(sample.split('/')[-1].split('.')[0:-2]) + '_fastqc.html'
+        out_qc_raw_fastq = ('.').join(sample.split(
+            '/')[-1].split('.')[0:-2]) + '_fastqc.html'
         out_qc_raw_file_fastq = os.path.join(out_fastqc_dir, out_qc_raw_fastq)
 
         if os.path.isfile(out_qc_raw_file_fastq):
-            logger.info(YELLOW + DIM + out_qc_raw_file_fastq + ' EXIST\nOmmiting QC for sample ' + sample_base + END_FORMATTING)
+            logger.info(YELLOW + DIM + out_qc_raw_file_fastq +
+                        ' EXIST\nOmmiting QC for sample ' + sample_base + END_FORMATTING)
         else:
-            logger.info(GREEN + 'Checking quality in sample ' + sample_base + END_FORMATTING)
+            logger.info(GREEN + 'Checking quality in sample ' +
+                        sample_base + END_FORMATTING)
             fastqc_quality(sample, out_fastqc_dir, args.threads)
-
 
     logger.info("\n" + MAGENTA + BOLD +
                 "#####END OF PIPELINE#####" + END_FORMATTING + "\n")
